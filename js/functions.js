b3e4c05172245934a8a0d377271209e168c4cbc9
Alina Zharkova <alinechipollino@gmail.com>
2024-03-05 17:24:56 +0300

    fix personalities and add arrow-up

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,21 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 // function footer() {
--- js/persons-slider.js
+++ js/persons-slider.js
@@ -1,54 +1,56 @@
-$(document).ready(function(){
-  $("#persons-carousel").owlCarousel({
-      nav: false,
-      dots: true,
-      responsive: {
-          0: {
-              items: 1,
-              stagePadding: 50,
-              center: true,
-              margin: 20,
-          },
-          600: {
-              items: 2,
-              stagePadding: 50,
-              center: true,
-              margin: 20,
-          },
-          1000: {
-              items: 4,
-              margin: 10,
-          }
-      }
-  });
-});
+window.onload = function() {
+    $(document).ready(function(){
+    $("#persons-carousel").owlCarousel({
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 50,
+                center: true,
+                margin: 20,
+            },
+            600: {
+                items: 2,
+                stagePadding: 50,
+                center: true,
+                margin: 20,
+            },
+            1000: {
+                items: 4,
+                margin: 10,
+            }
+        }
+    });
+    });
 
-$(document).ready(function() {
-  // Функция для выравнивания высоты элементов слайдера
-  function alignSlideHeights() {
-      var slides = document.querySelectorAll('.person-card');
-      var maxHeight = 0;
+    $(document).ready(function() {
+    // Функция для выравнивания высоты элементов слайдера
+    function alignSlideHeights() {
+        var slides = document.querySelectorAll('.person-card');
+        var maxHeight = 0;
 
-      // Находим максимальную высоту элемента слайдера
-      slides.forEach(function(slide) {
-          var slideHeight = slide.clientHeight;
-          if (slideHeight > maxHeight) {
-              maxHeight = slideHeight;
-          }
-      });
+        // Находим максимальную высоту элемента слайдера
+        slides.forEach(function(slide) {
+            var slideHeight = slide.clientHeight;
+            if (slideHeight > maxHeight) {
+                maxHeight = slideHeight;
+            }
+        });
 
-      // Применяем максимальную высоту ко всем элементам слайдера
-      slides.forEach(function(slide) {
-          slide.style.height = maxHeight + 'px';
-      });
-  }
+        // Применяем максимальную высоту ко всем элементам слайдера
+        slides.forEach(function(slide) {
+            slide.style.height = maxHeight + 'px';
+        });
+    }
 
-  // Выравниваем высоту при загрузке страницы
-  alignSlideHeights();
+    // Выравниваем высоту при загрузке страницы
+    alignSlideHeights();
 
-  // Выравниваем высоту при изменении размера экрана
-  window.addEventListener('resize', function() {
-      alignSlideHeights();
-  });
-});
+    // Выравниваем высоту при изменении размера экрана
+    window.addEventListener('resize', function() {
+        alignSlideHeights();
+    });
+    });
+}
   
