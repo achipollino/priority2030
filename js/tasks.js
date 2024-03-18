703c3d092746de4602c1d6cc09af2466dfad5f4e
Alina Zharkova <alinechipollino@gmail.com>
2024-03-19 00:53:13 +0300

    feat transformation

--- js/tasks.js
+++ js/tasks.js
@@ -1,21 +1,33 @@
+'strict'
+
 function openTab(evt, tabName) {
-        var i, tabcontent, tablinks;
-  
-        // Получим все элементы с классом tabcontent и спрячем их
-        tabcontent = document.getElementsByClassName(".tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-    
-        // Получим все элементы с классом tablinks и удалим активный класс
-        tablinks = document.getElementsByClassName(".tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(".active", " ");
-        }
-    
-        // Покажем текущую вкладку и добавим активный класс на кнопку, которая откроет вкладку с ID по названию
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += ".active";
+  // Получим все элементы с классом tabcontent и спрячем их
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none"; 
+  }
+
+  // Получим все элементы с классом tablinks и удалим активный класс
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+  }
+
+  // Покажем текущую вкладку и добавим активный класс на кнопку, которая откроет вкладку с ID по названию
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
 }
 
-  
+// Вызываем функцию openTab сразу после загрузки страницы для активации вкладки "Management"
+window.onload = function() {
+  openTab(document.getElementById("defaultOpen"), 'Management');
+};
+
+function getCurrentEvent(objectEvent) {
+  var targ;
+  if (!objectEvent) var objectEvent = window.event;
+  if (objectEvent.target) targ = objectEvent.target;
+  else if (objectEvent.srcElement) targ = objectEvent.srcElement;
+  if ('nodeType' in objectEvent)
+    if (objectEvent.nodeType == 3) targ = targ.parentNode; // defeat Safari bug
+}
