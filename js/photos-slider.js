30702780c2ed6c7cd605567cc5b0a504fcfe8759
Alina Zharkova <81089986+achipollino@users.noreply.github.com>
2024-03-26 02:14:41 +0300

    feat: create politic pages

---
+++ js/photos-slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#photos-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3,
+                margin: 60,
+            }
+        }
+    });
+});
